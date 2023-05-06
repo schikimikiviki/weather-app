80e7084e9142fa287c8416fa03748d9ad806c6e3
hansdav <hdavidson21@gmail.com>
2023-05-06 11:42:11 +0200

    dailyforecast component without css

---
+++ my-app/components/DailyForcast.jsx
@@ -0,0 +1,17 @@
+const DailyForecast = (weatherData) => {
+	return (
+		<div className="container">
+			<div className="icon"></div>
+			<div className="data">
+				<span>Today</span>
+				<div className="city-name">{weatherData.city}</div>
+				<div className="temperature">
+					Temperature: {weatherData.temperature}{" "}
+				</div>
+				<div className="weather"> {weatherData.weather}</div>
+			</div>
+		</div>
+	);
+};
+
+export default DailyForecast;
