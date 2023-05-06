893472963f0c1b1153f5163f44582e056d797529
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-05-06 11:13:10 +0200

    app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [background, setBackground] = useState('');
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ backgroundImage: background }} className='mainpage-bg'></div>
   );
 }
 
