411bcc541e23cb16d70ffecca828a9b3b6345685
hansdav <hdavidson21@gmail.com>
2023-05-06 13:04:36 +0200

    deleted old app.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
-import DailyForecast from "./components/DailyForecast.jsx";
+import { useState, useEffect } from "react";
 import fetchWeather from "./API/fetchWeather.js";
-
-fetchWeather("tokyo");
+import DailyForecast from "./components/DailyForecast";
 
 function App() {
-	return <DailyForecast />;
+	const [background, setBackground] = useState("");
+	const [city, setCity] = useState("Vienna");
+	const [weatherData, setWeatherData] = useState(null);
+
+	useEffect(() => {
+		const getWeatherData = async () => {
+			const data = await fetchWeather(city);
+			setWeatherData(data);
+		};
+		getWeatherData();
+	}, [city]);
+
+	useEffect(() => {
+		if (weatherData) {
+			// Update the background
+			setBackground();
+		}
+	}, [weatherData]);
+
+	return (
+		<div style={{ backgroundImage: background }} className="mainpage-bg">
+			<p>{weatherData ? weatherData.weather[0].description : "Loading..."}</p>
+			<DailyForecast></DailyForecast>
+		</div>
+	);
 }
 
 export default App;
