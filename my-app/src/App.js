cb8dccd29de53850069ae556df1d2860cb21bcf3
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-05-06 11:45:55 +0200

    app and fetch function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import fetchWeather from './API/fetchWeather.js';
 
 function App() {
   const [background, setBackground] = useState('');
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState('');
+  const [city, setCity] = useState('Vienna');
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const getWeatherData = async () => {
+      const data = await fetchWeather.fetchWeather(city);
+      setWeatherData(data);
+    };
+    getWeatherData();
+  }, [city]);
+
+  useEffect(() => {
+    if (weatherData) {
+      // Update the background
+      setBackground();
+    }
+  }, [weatherData]);
 
   return (
-    <div style={{ backgroundImage: background }} className='mainpage-bg'></div>
+    <div style={{ backgroundImage: background }} className='mainpage-bg'>
+      <p>{weatherData ? weatherData.weather[0].description : 'Loading...'}</p>
+    </div>
   );
 }
 
